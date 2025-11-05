e729dbd4afb23f1ebeb0890eb231f869d633e685
saurabh-pingale <saurabh.pingale@formpilot.org>
2025-11-05 22:01:43 +0530

    Added faraster configuration and disable (ts, lint).

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { sdk } from '@farcaster/frame-sdk';
 import { Game } from './components/Game';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    sdk.actions.ready();
+  }, []);
   return (
     <div className="App">
       <Game />
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  server: {
+    host: true,
+    allowedHosts: ['carrom-game-web.vercel.app'],
+  },
 })
 
