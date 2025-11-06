cbeaffa211bac7a855e29ffab8b516cd9fa37826
saurabh-pingale <saurabh.pingale@formpilot.org>
2025-11-06 12:08:40 +0530

    Fixed farcaster config.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
-import { sdk } from '@farcaster/frame-sdk';
+import { sdk } from '@farcaster/miniapp-sdk';
 import { Game } from './components/Game';
 import './App.css';
 
 function App() {
   useEffect(() => {
-    sdk.actions.ready();
+    (async () => {
+      await sdk.actions.ready();
+    })();
   }, []);
   return (
     <div className="App">
